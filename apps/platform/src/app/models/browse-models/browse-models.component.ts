f94b69f03383acf6418de8fe03908db13d34d3b4
Bilal Shaikh <bilalshaikh42@gmail.com>
2021-07-13 21:05:11 -0400

    feat: wip platform

--- apps/platform/src/app/models/browse-models/browse-models.component.ts
+++ apps/platform/src/app/models/browse-models/browse-models.component.ts
@@ -6,11 +6,7 @@ import {
   ViewChild,
 } from '@angular/core';
 import { ModelData, ModelDataSource } from './models-datasource';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { MatTable } from '@angular/material/table';
-import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
-import { Observable } from 'rxjs';
+import { BrowseModelsService } from './model.service';
 import { Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'biosimulations-browse-models',
@@ -19,98 +15,20 @@ import { Router, ActivatedRoute } from '@angular/router';
   providers: [ModelDataSource],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BrowseModelsComponent implements AfterViewInit, OnInit {
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort;
-  @ViewChild(MatTable) table!: MatTable<ModelData>;
-  showMenu = true;
-  isLoading!: Observable<boolean>;
-
-  data: ModelData[] = [];
-  data$: any;
-  displayedColumns: any[] = [
-    { id: 'id', show: true, minWidth: 130, maxWidth: null, nowrap: true },
-    { id: 'name', show: true, minWidth: null, maxWidth: null, nowrap: false },
-    { id: 'taxon', show: true, minWidth: 75, maxWidth: null, nowrap: false },
-    { id: 'tags', show: true, minWidth: null, maxWidth: null, nowrap: false },
-    {
-      id: 'framework',
-      show: true,
-      minWidth: 77,
-      maxWidth: null,
-      nowrap: false,
-    },
-    {
-      id: 'format',
-      show: false,
-      minWidth: null,
-      maxWidth: null,
-      nowrap: false,
-    },
-    {
-      id: 'authors',
-      show: true,
-      minWidth: null,
-      maxWidth: null,
-      nowrap: false,
-    },
-    { id: 'owner', show: false, minWidth: null, maxWidth: null, nowrap: false },
-    { id: 'license', width: 1, minWidth: 96, maxWidth: null, nowrap: false },
-    { id: 'created', show: false, minWidth: 60, maxWidth: null, nowrap: false },
-    { id: 'updated', show: true, minWidth: 60, maxWidth: null, nowrap: false },
-  ];
-  columnsToDisplay: string[] = this.displayedColumns
-    .filter((col) => col.show)
-    .map((col) => col.id);
-  initCheckbox: { [key: string]: boolean } = {};
+export class BrowseModelsComponent {
   constructor(
-    public dataSource: ModelDataSource,
     private router: Router,
     private route: ActivatedRoute,
+    private service: BrowseModelsService,
   ) {}
-  ngAfterViewInit(): void {
-    this.dataSource.sort = this.sort;
-    this.dataSource.paginator = this.paginator;
-    this.table.dataSource = this.dataSource;
+  public data$;
+  private ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.data$ = this.service.getModels();
   }
-
-  ngOnInit(): void {
-    this.displayedColumns.map(
-      (value: any) => (this.initCheckbox[value.id] = value.show),
-    );
-
-    this.isLoading = this.dataSource.isLoading$();
-  }
-  toggleMenu() {
-    this.showMenu = !this.showMenu;
-  }
-  handleChange(checked: boolean, columnId: string) {
-    if (checked) {
-      for (let iColumn = 0; iColumn < this.displayedColumns.length; iColumn++) {
-        if (this.displayedColumns[iColumn].id === columnId) {
-          this.columnsToDisplay.splice(iColumn, 0, columnId);
-          break;
-        }
-      }
-    }
-    if (!checked) {
-      this.columnsToDisplay.splice(this.columnsToDisplay.indexOf(columnId), 1);
-    }
-  }
-  drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(
-      this.columnsToDisplay,
-      event.previousIndex,
-      event.currentIndex,
-    );
-  }
-
-  navigate(el: ModelData) {
-    const id = el.id;
+  navigate() {
+    const id = '';
     this.router.navigate(['../' + id], { relativeTo: this.route });
-  }
-
-  refresh() {
-    this.dataSource.refresh();
   }
 }
